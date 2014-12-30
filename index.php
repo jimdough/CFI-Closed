746c6f43ff82a50ea9bc11a4cb7263e8e2755bac
Jim D <jimdough@MacBook-Pro.local>
2014-12-30 16:44:58 -0500

    CFI Closed Landing Page

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Central Florida Institute Medical & Dental Training</title>
+
+    <?php include("inc/head2.php"); ?>
+    <title>CFI Information - Central Florida Institute Medical and Dental Training Schools of Orlando and Palm Harbor</title>
+    </head>
+  	<body>
+
+    <div class="row" id="panel1">
+    <div class="container">
+    <div class="col-md-6 pull-left" id="logoWrap">
+    	<a href="index.php"><img class="img-responsive" id="logo" src="images/logo.png" border="0"></a>
+    </div>
+   
+   <div class="col-md-6 pull-right hidden-sm" id="topPhone">
+    	<h2>1-888-831-8303</h2>
+    	<?php include("inc/ctc.inc"); ?>
+    </div>
+    </div>
+    </div>
+    
+
+
+<div class="row" id="interior">
+    <div class="container">
+  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> <!--Left-->
+
+  <!-- Insert Content Here -->
+  
+  <p>Central Florida Institute has voluntarily closed both of its campuses effective November 1, 2014.</p>
+<p>If you are a graduate or former student and would like to receive a transcript of your completed course work at either CFI campus, please request in writing via email CFIrecords@careerpathtraining.com or via USPS:</p>
+<br>
+CFI Records
+<br>
+Career Path Training
+<br>
+11300 4th Street North
+<br>
+St. Petersburg, FL  33716
+<br><br>
+<p>For your convenience, a transcript request form is available by <a href="transcript.pdf" target="_blank">clicking here.</a></p>
+<p>
+Processing fee is $7 per copy made payable to Career Path Training. </p> 
+
+<p>CFI has continuing education partners who will accept all or some of the course work graduates completed at CFI.  The list below includes those partners and their contact information.</p>
+<ul>
+	<li><a href="rasmussen.php" target="_blank">Rasmussen College</a></li>
+	<li><a href="nsu.php" target="_blank">Nova Southeastern University</a></li>
+	<li><a href="nlu.php" target="_blank">National Louis University</a></li>
+	<li><a href="ghe.php" target="_blank">Global Health Education</a></li>
+</ul>
+<p>Employers who are seeking proof of education of a former CFI Graduate may contact us via <a href="mailto:CFIRecords@careerpathtraining.com">CFIRecords@careerpathtraining.com</a> or:</p>
+<br> 
+CFI Records
+<br>
+Career Path Training
+<br>
+11300 4th Street North
+<br>
+St. Petersburg, FL  33716
+<br>
+727-342-6420 x232
+<br><br>
+<p>For General Information or Questions please call 727-342-6420 x232 or email us at <a href="mailto:CFIRecords@careerpathtraining.com">CFIRecords@careerpathtraining.com</a>.</p>
+
+  
+    
+  </div> <!-- /column -->
+
+<!--Right-->
+
+  
+<!-- /column -->
+  
+    </div> <!-- /.container -->
+</div> <!-- End Tentpoles --><!-- / row-->
+  
+  <div class="row shadow" id="footer">
+  	<div class="container">
+    <?php include("inc/footer.inc"); ?>
+	</div><!-- /.container-->
+   </div><!-- / row-->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+            <?php include("inc/scripts.inc"); ?>
+  </body>
+</html>
---
+++ nsu.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php include("inc/head2.php"); ?>
+
+    <title>Central Florida Institute Medical & Dental Training</title>
+
+       </head>
+  	<body>
+
+    <div class="row" id="panel1">
+    <div class="container">
+    <div class="col-md-6 pull-left" id="logoWrap">
+    	<a href="index.php"><img class="img-responsive" id="logo" src="images/logo.png" border="0"></a>
+    </div>
+   
+   <div class="col-md-6 pull-right hidden-sm" id="topPhone">
+    	<h2>1-888-831-8303</h2>
+    	<?php include("inc/ctc.inc"); ?>
+    </div>
+    </div>
+    </div>
+    
+
+
+<div class="row" id="interior">
+    <div class="container">
+  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> <!--Left-->
+  
+   <h3>Nova Southeastern University partners with Central Florida Institute</h3>
+
+  <!-- Insert Content Here -->
+  <p>Learn How your Central Florida Institute credits can transfer into Nova Southeastern University’s Bachelor of Health Science Program</p>
+  <ul>
+	  <li>Regionally Accredited</li>
+	  <li>Professors are Practitioners</li>
+	  <li>Program is entirely online</li>
+  </ul>
+
+<p><a href="http://www.nova.edu/campuses/tampa/"><img border="0" style="padding:10px;" alt="NOVA Logo" src="images/nsu.jpg"  align="right" /></a>Central Florida Institute is working with Nova Southeastern University to help our students continue their education. Their flexible formats work with your schedule and allow you to earn your Bachelor's, Master's. Specialist or Doctoral degree onsite or online in fields such as:</p>
+
+<ul>
+<li>Anesthesiologist Assistant</li>
+<li>Business Administration</li>
+<li>Business Management</li>
+<li>Cardiovascular Sonography</li>
+<li>Cross Disciplinary Studies</li>
+<li>Education</li>
+<li>Higher Education</li>
+<li>Interdisciplinary Arts</li>
+<li>Management Information Systems</li>
+<li>Mental Health Counseling</li>
+<li>Nursing</li>
+<li>Occupational Therapy</li>
+<li>Physical Therapy</li>
+<li>School Counseling</li>
+<li>School Psychology</li>
+<li>Speech-Language Pathology</li>
+</ul>
+
+<h3>Contact</h3> 
+<p>Kimberly Shaw<br>
+Assistant Director for Enrollment and Student Recruitment<br>
+Phone 813-574-5271<br>
+<a href="mailto:kimberly.shaw@nova.edu">kimberly.shaw@nova.edu</a>
+
+<h3>About Nova Southeastern University</h3>
+
+<p>The Tampa Campus is conveniently located near I-75. All programs are offered in accessible formats to accommodate your demanding schedule. The center contains 81,500 square feet of space. The Tampa Campus supports many area organizations & provides a great meeting space.</p>
+<p><a target="_blank" href="http://www.nova.edu/campuses/tampa/">Website</a></p>
+
+<p class="disclaimer">* College Level English must have been completed at a regionally accredited college or have successfully passed the College Composition CLEP exam<br>* Must have successfully passed either the CCI or ARDMS exams</p>
+
+  </div> <!-- /column -->
+
+<!--Right-->
+
+  
+<!-- /column -->
+  
+    </div> <!-- /.container -->
+</div> <!-- End Tentpoles --><!-- / row-->
+  
+  <div class="row shadow" id="footer">
+  	<div class="container">
+    <?php include("inc/footer.inc"); ?>
+	</div><!-- /.container-->
+   </div><!-- / row-->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+            <?php include("inc/scripts.inc"); ?>
+  </body>
+</html>
